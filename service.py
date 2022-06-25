4ae713ade868bbc5366f9f4193b5a93f4e188095
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-25 23:15:49 +0900

    fresh init

--- service.py
+++
@@ -1,8 +0,0 @@
-import repository
-
-def add_user(email: str, password: str) -> str:
-    try:
-        repository.insert_user(email, password)
-    except:
-        return "Cannot sign up"
-    return "null"
--- unit_testing.py
+++
@@ -1,4 +0,0 @@
-import service
-
-# TC1: add user
-print(service.add_user("ewqdsacxz1234@gmail.com", "toor"))
