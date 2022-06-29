1d38e7f5a755759f4e5c0ac7da14dfdbc27e3033
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-29 23:21:28 +0900

    feat: Note schema

---
+++ note/schemas/note.schema.go
@@ -0,0 +1,12 @@
+package schemas
+
+// AWS DDB item schema
+type Note struct {
+    Id      string      // AWS DDB partition key
+    Title   string      // AWS DDB attribute
+    Code    string      // AWS DDB attribute
+    Owner   string      // AWS DDB attribute
+    Tags    []string    // AWS DDB attribute
+    Refs    []string    // AWS DDB attribute
+    Scope   uint16      // AWS DDB attribute
+}
